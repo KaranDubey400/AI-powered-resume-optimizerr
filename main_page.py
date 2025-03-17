e8b20aefa2b370f0a9d95f5928d7b67bc2dd30c7
KARN <95653930+KaranDubey400@users.noreply.github.com>
2025-03-17 14:29:49 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,159 +1,22 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import firebase_admin
-from firebase_admin import credentials
-import json
-import requests
 
-# Initialize Firebase only if it's not already initialized
-if not firebase_admin._apps:
-    firebase_config = {
-        "type": st.secrets["type"],
-        "project_id": st.secrets["project_id"],
-        "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"],
-        "client_email": st.secrets["client_email"],
-        "client_id": st.secrets["client_id"],
-        "auth_uri": st.secrets["auth_uri"],
-        "token_uri": st.secrets["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
-    }
-    cred = credentials.Certificate(firebase_config)
-    firebase_admin.initialize_app(cred)
+# From files
+from Frontend.home_page import home as HP
+from Frontend.instruction_page import Instruction as Ins
+from Frontend.ats_page import Ats_page as ats
+from Frontend.about import About_section as Ab_sec
 
+# Set page config at the very top
+st.set_page_config(layout="wide")
 
-# Sign-Up/Sign-In Functions
-def sign_up_with_email_and_password(email, password, username=None, return_secure_token=True):
-    try:
-        rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
-        payload = {
-            "email": email,
-            "password": password,
-            "returnSecureToken": return_secure_token
-        }
-        if username:
-            payload["displayName"] = username
-        payload = json.dumps(payload)
-        r = requests.post(rest_api_url, params={"key": st.secrets["firebase_api_key"]}, data=payload)
-        try:
-            return r.json()['email']
-        except:
-            st.warning(r.json())
-    except Exception as e:
-        st.warning(f'Signup failed: {e}')
+# Sidebar Image - Load Only Once
+if "sidebar_image_loaded" not in st.session_state:
+    st.session_state["sidebar_image_loaded"] = True
+    with st.sidebar:
+        st.image("assets/logo/grey.png", use_column_width=True)
 
-
-def sign_in_with_email_and_password(email=None, password=None, return_secure_token=True):
-    rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
-    try:
-        payload = {
-            "returnSecureToken": return_secure_token
-        }
-        if email:
-            payload["email"] = email
-        if password:
-            payload["password"] = password
-        payload = json.dumps(payload)
-        r = requests.post(rest_api_url, params={"key": st.secrets["firebase_api_key"]}, data=payload)
-        try:
-            data = r.json()
-            user_info = {
-                'email': data['email'],
-                'username': data.get('displayName')  # Retrieve username if available
-            }
-            return user_info
-        except:
-            st.warning(data)
-    except Exception as e:
-        st.warning(f'Signin failed: {e}')
-
-
-def reset_password(email):
-    try:
-        rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode"
-        payload = {
-            "email": email,
-            "requestType": "PASSWORD_RESET"
-        }
-        payload = json.dumps(payload)
-        r = requests.post(rest_api_url, params={"key": st.secrets["firebase_api_key"]}, data=payload)
-        if r.status_code == 200:
-            return True, "Reset email Sent"
-        else:
-            # Handle error response
-            error_message = r.json().get('error', {}).get('message')
-            return False, error_message
-    except Exception as e:
-        return False, str(e)
-
-
-# Authentication Page
-def authentication_page():
-    st.title('Welcome to :violet[Craftify] :sunglasses:')
-
-    if 'username' not in st.session_state:
-        st.session_state.username = ''
-    if 'useremail' not in st.session_state:
-        st.session_state.useremail = ''
-
-    def f():
-        try:
-            userinfo = sign_in_with_email_and_password(st.session_state.email_input, st.session_state.password_input)
-            st.session_state.username = userinfo['username']
-            st.session_state.useremail = userinfo['email']
-            st.session_state.signedout = False
-        except:
-            st.warning('Login Failed')
-
-    def forget():
-        email = st.text_input('Email')
-        if st.button('Send Reset Link'):
-            success, message = reset_password(email)
-            if success:
-                st.success("Password reset email sent successfully.")
-            else:
-                st.warning(f"Password reset failed: {message}")
-
-    if "signedout" not in st.session_state:
-        st.session_state["signedout"] = True
-
-    if st.session_state["signedout"]:
-        choice = st.selectbox('Login/Signup', ['Login', 'Sign up'])
-        email = st.text_input('Email Address')
-        password = st.text_input('Password', type='password')
-        st.session_state.email_input = email
-        st.session_state.password_input = password
-
-        if choice == 'Sign up':
-            username = st.text_input("Enter your unique username")
-            if st.button('Create my account'):
-                sign_up_with_email_and_password(email=email, password=password, username=username)
-                st.success('Account created successfully!')
-                st.markdown('Please Login using your email and password')
-                st.balloons()
-        else:
-            st.button('Login', on_click=f)
-            forget()
-
-    else:
-        st.text('Name: ' + st.session_state.username)
-        st.text('Email id: ' + st.session_state.useremail)
-        if st.button('Sign out'):
-            st.session_state.signedout = True
-            st.session_state.username = ''
-            st.session_state.useremail = ''
-            st.rerun()
-
-        # Add a new button to go to the main app
-        if st.button('Go to the App →'):
-            st.session_state['page'] = 'main_app'
-            st.rerun()
-
-# Main Page
-def main_page():
-    st.set_page_config(layout="wide")
-
+def main():
     # Hide the Streamlit menu and footer
     hide_menu = """
         <style>
@@ -163,37 +26,56 @@ def main_page():
         """
     st.markdown(hide_menu, unsafe_allow_html=True)
 
-    # Sidebar with Sign Up and Sign Out buttons
-    with st.sidebar:
-        st.image("assets/logo/Colorlogo.png")
-
-        # Determine which button to show based on session state
-        if 'username' in st.session_state and st.session_state['username']:
-            if st.button("Log Out || mainpage"):
-                st.session_state['page'] = 'authentication'
-                st.rerun()
-        else:
-            if st.button("main page"):
-                st.session_state['page'] = 'authentication'
-                st.rerun()
-
-    # Main content display
-    if 'page' in st.session_state:
-        if st.session_state['page'] == 'authentication':
-            authentication_page()  # Render the authentication page
-        elif st.session_state['page'] == 'main_app':
-            import streamlit_app  # Dynamically load the main app
-            streamlit_app.main()
-        else:
-            st.write("Welcome to Craftify...  Get Noticed, Get Hired!")
-            st.image("assets/logo/karn.jpg", use_column_width='auto')
+    if st.session_state.get('switch_button', False):
+        st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 5
+        manual_select = st.session_state['menu_option']
     else:
-        # Display the main page content if no specific page is set
-        # st.write("Understand Job Requirements. Tweak Your Resume. Shoot Your Shot!")
-        st.markdown("<h1 style='text-align: center; font-size: 50px; text-transform: uppercase;'>UNDERSTAND JOB REQUIREMENTS. TWEAK YOUR RESUME. APPLY!</h1>", unsafe_allow_html=True)
+        manual_select = None
 
-        st.image("assets/logo/karn.jpg", use_column_width='auto')
+    selected_main = option_menu(None, ["Home", "Instruction", "ATS Analyzer", "About"],
+                                icons=['house', 'folder', 'cloud', 'person', 'gear'],
+                                orientation="horizontal", manual_select=manual_select, key='menu_4')
 
+    if selected_main == "Home":
+        HP.home_page()
+    elif selected_main == "Instruction":
+        Ins.instruction()
+    elif selected_main == "ATS Analyzer":
+        ats.resume_parser()
+    elif selected_main == "About":
+        Ab_sec.About_Section()
+
+    # Create an empty space to center the button horizontally
+    col1, col2, col3 = st.columns([7, 5, 5])
+
+    # Place the button inside the middle column
+    with col2:
+        st.button(f"Next Page", key='switch_button')
+
+    # Footer note
+    footer_html = """
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: -9px;
+            width: 100%;
+            background-color: #C8A1E0;
+            display:flex;
+            flex-direction: column;
+            align-items:center;
+        }
+        .footer b {
+            color: #33372C;
+            font-size: 20px;
+        }
+    </style>
+    <div class="footer">
+        <b>Craftify</b>
+        <p>karn</p>
+    </div>
+    """
+    st.markdown(footer_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    main_page()
+    main()
